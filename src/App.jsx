666dfc349d29edced7478743c10684af16029c7f
SATHESH KUMAR B <Sathesh15601@gmail.com>
2025-06-09 11:04:08 +0530

    reload bugs clear and next we need to add some animations

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  base: "/BSK-PORTFOLIO/", // 👈 match your GitHub repo name exactly!
+  plugins: [react(), tailwindcss()],
 });
